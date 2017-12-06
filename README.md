ba27747edbc2712fe687d5194ba2672c84ecfeca
PavelShturm85 <pavel.shturm@gmail.com>
2017-12-06 20:16:25 +0400

    Добавлена возможность выбора дней, изменены названия функций.

--- README.md
+++ README.md
@@ -2,16 +2,28 @@
 
 **The module check sites health**
 
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+*Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.*
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
 # Quickstart
 **Ways to use:**
 - Have to use  module `check_sites_health.py` after `python3` with `url.txt`.
+  - You get 30 days before expiration  by default.
+  - You may get other amount days, if use `-d`.
 
 
 Example of script launch on Linux, Python 3.5:
 
 
 ```bash
-$ python3 check_sites_health.py url.txt
+$ python3 check_sites_health.py url.txt -d 50
 Domain:http://linux.org is OK!
 Domain:https://pythonworld.ru is OK!
 Domain:https://habrahabr.ru is OK!
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,51 +1,63 @@
 import requests
 import sys
+import argparse
 from datetime import datetime
 from whois import whois
 
 
-def load_urls4check(enter_file):
-    with open(enter_file, 'r') as urls4check:
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='Module for check sites health.')
+    parser.add_argument(
+        'domain_file', help='Where get file with domain.')
+    parser.add_argument(
+        '-d', '--days', default=30, type=int,
+        help='Input days before expiration.')
+    return parser
+
+
+def load_urls4check(domain_file):
+    with open(domain_file, 'r') as urls4check:
         return urls4check.read().splitlines()
 
 
 def is_server_respond_with_200(domain_list):
     server_respond_with_200_list = []
     for url in domain_list:
-        code = requests.get('{}'.format(url)).status_code
+        code = requests.get(url).status_code
         if code == 200:
             server_respond_with_200_list.append(url)
     return server_respond_with_200_list
 
 
-def get_domain_expiration_normal(domain_list):
+def get_domain_expiration_normal(domain_list, days_before_expiration):
     domain_expiration_normal = []
     for url in domain_list:
         delta = whois(url).expiration_date - datetime.now()
-        if delta.days >= 30:
+        if delta.days >= days_before_expiration:
             domain_expiration_normal.append(url)
     return domain_expiration_normal
 
 
-def print_result(domains_list, domain_properties):
+def print_health_domain(domains_list, domain_properties):
     for url in domains_list:
         print('Domain:{} {}!'.format(url, domain_properties))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        input_file_name = sys.argv[1]
-    else:
-        input_file_name = input('Введите путь к файлу: ')
+    parser = create_parser()
+    namespace = parser.parse_args()
 
-    enter_file = load_urls4check(input_file_name)
-    expiration_normal = get_domain_expiration_normal(enter_file)
-    respond_200 = is_server_respond_with_200(enter_file)
+    domain_file = load_urls4check(namespace.domain_file)
+    expiration_normal = get_domain_expiration_normal(
+        domain_file, namespace.days)
+    respond_200 = is_server_respond_with_200(domain_file)
 
     actualy_domain = list(set(expiration_normal) & set(respond_200))
-    expiration_not_normal = list(set(enter_file) - set(expiration_normal))
-    not_respond_200 = list(set(enter_file) - set(respond_200))
+    expiration_not_normal = list(set(domain_file) - set(expiration_normal))
+    not_respond_200 = list(set(domain_file) - set(respond_200))
 
-    print_result(actualy_domain, 'is OK')
-    print_result(expiration_not_normal, 'has problem with expirationdate')
-    print_result(not_respond_200, 'is NOT ok')
+    print_health_domain(actualy_domain, 'is OK')
+    print_health_domain(expiration_not_normal,
+                        'has problem with expiration date')
+    print_health_domain(not_respond_200, 'is NOT ok')
